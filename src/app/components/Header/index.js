eb9ff0ffe784f71751943dcf174a8787634eab6e
Eduardo Griesang <eduardogriesang1@gmail.com>
2024-03-05 15:44:26 -0300

    feat: color fixes

--- src/app/components/Header/index.js
+++ src/app/components/Header/index.js
@@ -49,16 +49,16 @@ export default function Header () {
                     <div className={`${mobileMenu} justify-between items-center w-full lg:flex lg:w-auto lg:order-1" id="mobile-menu-2`}>
                         <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
                             <li>
-                                <a href="#" className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-400 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-400 rounded-sm">Início</a>
+                                <a href="#" className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-black lg:dark:hover:text-white dark:hover:bg-gray-400 dark:hover:text-white lg:dark:hover:bg-transparent lg:dark:text-white dark:border-gray-400 rounded-sm">Início</a>
                             </li>
                             <li>
-                                <a href="#estoque" className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-400 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-400 rounded-sm">Estoque</a>
+                                <a href="#estoque" className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-black lg:dark:hover:text-white dark:hover:bg-gray-400 dark:hover:text-white lg:dark:hover:bg-transparent lg:dark:text-white dark:border-gray-400 rounded-sm">Estoque</a>
                             </li>
                             <li>
-                                <a href="#sobre" className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-400 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-400 rounded-sm">Sobre nós</a>
+                                <a href="#sobre" className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-black lg:dark:hover:text-white dark:hover:bg-gray-400 dark:hover:text-white lg:dark:hover:bg-transparent lg:dark:text-white dark:border-gray-400 rounded-sm">Sobre nós</a>
                             </li>
                             <li>
-                                <a href="#contato" className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-400 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-400 rounded-sm">Contato</a>
+                                <a href="#contato" className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-black lg:dark:hover:text-white dark:hover:bg-gray-400 dark:hover:text-white lg:dark:hover:bg-transparent lg:dark:text-white dark:border-gray-400 rounded-sm">Contato</a>
                             </li>
                         </ul>
                     </div>
